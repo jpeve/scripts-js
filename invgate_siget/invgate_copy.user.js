91df761b8581cd4a33df83ebb7f48764cbebf04f
Javier Peverelli <javier.peverelli@gmail.com>
2021-10-05 10:46:27 -0300

    0.4

--- invgate_siget/invgate_copy.user.js
+++ invgate_siget/invgate_copy.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         InvGate Copy SIGET
 // @namespace    ar.jpeve.copy
-// @version      0.1
+// @version      0.4
 // @description  Mejora: copia el contenido de un ticket de InvGate para luego pegarlo en Siget
 // @author       Javier Peverelli
 // @match        https://pau.osde.com.ar/requests/show/index/id/*
-// @updateURL    https://github.com/jpeve/scripts-js/raw/master/invgate_siget/invgate_copy.user.js
-// @downloadURL  https://github.com/jpeve/scripts-js/raw/master/invgate_siget/invgate_copy.user.js
+// @updateURL    https://github.com/jpeve/scripts-js/raw/main/invgate_siget/invgate_copy.user.js
+// @downloadURL  https://github.com/jpeve/scripts-js/raw/main/invgate_siget/invgate_copy.user.js
 // @grant        none
 // ==/UserScript==
 
--- invgate_siget/paste_siget.user.js
+++ invgate_siget/paste_siget.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         InvGate Paste SIGET
 // @namespace    ar.jpeve.paste
-// @version      0.1
+// @version      0.4
 // @description  Mejora: pega el contenido copiado de InvGate dentro de Siget.
 // @author       Javier Peverelli
 // @match        http://siget.softtekosde.com:8080/siget/*
-// @updateURL    https://github.com/jpeve/scripts-js/raw/master/invgate_siget/paste_siget.user.js
-// @downloadURL  https://github.com/jpeve/scripts-js/raw/master/invgate_siget/paste_siget.user.js
+// @updateURL    https://github.com/jpeve/scripts-js/raw/main/invgate_siget/paste_siget.user.js
+// @downloadURL  https://github.com/jpeve/scripts-js/raw/main/invgate_siget/paste_siget.user.js
 // @grant        none
 // ==/UserScript==
 
